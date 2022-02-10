99d58c7367979dc2934042b225b8debfa5caa5c4
R Mo <morici23@gmail.com>
2022-02-09 23:34:00 -0500

    Initialize the board model and auto populate HTML board-section with squares.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
-const BOARDHEIGHT = 16
-const BOARDWIDTH = 8
+/* -------------------------------  CONSTANTS  ----------------------------------------- */
+const TOTAL_ROWS = 16
+const TOTAL_COLS = 8
+/* -------------------------------  CACHED REFERENCES  -------------------------------- */
+const boardContainer = document.querySelector('.board-container')
+/* -------------------------------  Variables  -------------------------------- */
+const sqDivs = []
 
-// <div class="sq"></div> 
+function init() {
+    sqDivs = initBoardDivs()
+    // Create the board
+    // set starting viruses
+    // * dont reset the score
+}
+
+// Create element and append to boardContainer / Also push div elements into sqDivs array
 function initBoardDivs() {
-    const div = document.createElement('div')
+    const numberOfTiles = TOTAL_COLS * TOTAL_ROWS
+    for(let i = 0; i < numberOfTiles; i++) {
+        const div = document.createElement('div')
+        div.className = 'sq'
+        // looks like this -->  <div class="sq"></div>
+        boardContainer.append(div)
+        sqDivs.push(div)
+    }
+
 }
