1309055338aae464fbffabd02ff458e7e0a1f16a
R Mo <morici23@gmail.com>
2022-02-14 15:56:08 -0500

    Remove all matches from board

--- js/app.js
+++ js/app.js
@@ -5,8 +5,6 @@
  * hinge Node - Image a 2x2 box around a 1x2 pill, the hinge node is the bottom left corner of that box and all rotations pivot around it. That is the magique behind rotating.
  */
 
-// TODO: Exit rotation / movements back to handler so it can render
-// TODO: Fully implement blocked rotation
 // TODO: Place a pill and get a new pill
 
 /* -------------------------------  CACHED REFERENCES  -------------------------------- */
@@ -46,6 +44,7 @@ const boardModel = []; // The board model
 /* -------------------------------  Variables  -------------------------------- */
 let virusCount;
 let playerPill;
+let gameSpeed = 400;
 /* ------------------------------- 🎮 Player Pill 💊 -------------------------------- */
 class PlayerPill {
 
@@ -76,7 +75,7 @@ class PlayerPill {
 
     isColliding(node, position) {
         let npNode = getNodeFromBoardModelAt(position)
-        return (npNode === null || npNode === node.sibling) ? false : true
+        return (npNode === '-' || npNode === node.sibling) ? false : true
     }
 
     isInBounds(position) {
@@ -86,10 +85,6 @@ class PlayerPill {
             (position.row >= 0) &&
             (position.row < TOTAL_ROWS)
         )
-    }
-
-    isDoneMoving() {
-
     }
 
     lastRowCheck() {
@@ -112,6 +107,7 @@ class PlayerPill {
             if (!this.isInBounds(targetPosition)) {
                 return false
             }
+
             // Prevent player from moving into another node
             if (this.isColliding(n, targetPosition)) {
                 return false
@@ -121,6 +117,7 @@ class PlayerPill {
         this.updatePillPosition(() => {
             this.nodes.forEach(n => n.position = this.addPositions(n.position, positionOffset))
         })
+        return true
     }
 
     updateOrientation() { this.orientation *= -1 }
@@ -137,8 +134,8 @@ class PlayerPill {
         let positionRightOfHinge = this.addPositions(this.nodes[0].position, RIGHT)
         let positionLeftOfHinge = this.addPositions(this.nodes[0].position, LEFT)
 
-        if (getNodeFromBoardModelAt(positionRightOfHinge) !== null &&
-            getNodeFromBoardModelAt(positionLeftOfHinge) === null
+        if (getNodeFromBoardModelAt(positionRightOfHinge) !== '-' &&
+            getNodeFromBoardModelAt(positionLeftOfHinge) === '-'
         ) {
             if (direction === CLOCKWISE) {
                 this.updatePillPosition(() => {
@@ -156,9 +153,10 @@ class PlayerPill {
             }
         }
     }
-
+    // ! BUG
+    // When trying to rotate on first tow there is an error because the target is out of bounds. Does not break game.
     rotate(direction) {
-        // If horizontal, check above the hinge node, if it vertical, check to the right of the hinge node
+
         let rotationOffset = (this.orientation === HORIZONTAL) ? TOP : RIGHT
         let rotationTargetPosition = this.addPositions(this.nodes[0].position, rotationOffset)
 
@@ -232,22 +230,13 @@ function handleKeyPress(evt) {
 function init() {
     // Create the HTML (View) board
     initSqDivs()
-    // Create player
-    playerPill = new PlayerPill()
-    // Create the empty model board
+    // Init board model to all nulls
     initBoardModel()
-    playerPill.placePlayerPillOnBoardModel()
     // set starting viruses
-    virusCount = 4;
+    virusCount = 180;
     initViruses()
     // * dont reset the score
-
-    // Test moving the pill on its own
-    // If move returns false then the pill is down moving.
-    // let test = setInterval(() => {
-    //     playerPill.move(MOVE_DOWN)
-    //     render()
-    // }, 400)
+    render()
 }
 
 // Creates div elements and appends them to boardContainer
@@ -272,7 +261,7 @@ function initBoardModel() {
     for (let row = 0; row < TOTAL_ROWS; row++) {
         const columnsInRow = []
         for (let col = 0; col < TOTAL_COLS; col++) {
-            columnsInRow.push(null)
+            columnsInRow.push('-')
         }
         boardModel.push(columnsInRow)
     }
@@ -301,7 +290,7 @@ function renderBoard() {
     for (let row = 0; row < TOTAL_ROWS; row++) {
         for (let col = 0; col < TOTAL_COLS; col++) {
             sqDivs[row][col].className = 'sq'
-            if (boardModel[row][col] !== null) {
+            if (boardModel[row][col] !== '-') {
                 let node = boardModel[row][col]
                 sqDivs[row][col].classList.add(node.color)
             }
@@ -339,10 +328,15 @@ function getRandomizedVirusNode(handicap) {
     }
 }
 
+function spawnPlayerPill() {
+    playerPill = new PlayerPill()
+    playerPill.placePlayerPillOnBoardModel()
+}
+
 /* -------------------------------  Helpers  -------------------------------- */
 function addNodeToBoardModel(node) { boardModel[node.position.row][node.position.col] = node }
 
-function removeNodeFromBoardModel(node) { boardModel[node.position.row][node.position.col] = null }
+function removeNodeFromBoardModel(node) { boardModel[node.position.row][node.position.col] = '-' }
 
 function clampNum(num, min, max) {
     if (num > max) { return max }
@@ -357,13 +351,142 @@ function logBoard() {
     boardModel.forEach(r => {
         let str = ''
         r.forEach(c => {
-            str += (c === null) ? '- ' : c.color + ' '
+            str += (c === '-') ? '- ' : c.color + ' '
         })
         console.log(str)
     })
     console.log('=====================')
 }
 
+function isGameOver() {
+    return (getNodeFromBoardModelAt(SPAWN_POSITION_A) !== '-' || 
+            getNodeFromBoardModelAt(SPAWN_POSITION_B) !== '-' ) ? true : false 
+}
+
+
+// ! ---- BUG 
+// TODO: fix bug that skips the first pill positions render.
+function runGameLoop() {
+    // check for game over
+    if(isGameOver) {
+        // spawn a pill
+        playerPill = new PlayerPill()
+        let gameLoop = setInterval(() => {
+            // move the pill
+            let moveResult = playerPill.move(BOTTOM)
+            if(moveResult === false) {
+                // Spawn a new pill
+                playerPill = new PlayerPill()
+            }
+            // 1 check for connections
+            // 2 clear connections (remember to decouple)
+            // 3 drop floating nodes
+            // Repeat 1 - 3 until there are no more connections
+            render()
+        }, gameSpeed)
+    }
+}
+
 /* -------------------------------  Main  -------------------------------- */
 init()
-render()
+
+/* -------------------------------  Import from sandbox  ----------------------*/
+
+function getAllMatchingPositionsOnBoard(array2D) {
+    let matchingPositions = []
+    // For each array in the 2d array, get character matches
+    for(let i = 0; i < array2D.length; i++) {
+        let indexOfMatches = getIndexesOfMatchingNodesFromArray(array2D[i])
+        indexOfMatches.forEach(matchIdx => {
+            // Convert matches into a position object
+            matchingPositions.push(getPositionObj(i,matchIdx))
+        })
+    }
+    return matchingPositions
+}
+
+function getAllMatchingPositionsOnBoardCol(array2D) {
+    let matchingPositions = []
+    // For each array in the 2d array, get character matches
+    for(let i = 0; i < array2D.length; i++) {
+        let indexOfMatches = getIndexesOfMatchingNodesFromArray(array2D[i])
+        indexOfMatches.forEach(matchIdx => {
+            // Convert matches into a position object
+            matchingPositions.push(getPositionObj(matchIdx,i))
+        })
+    }
+    return matchingPositions
+}
+
+function removeMatchesFromBoard() {
+    // find all the matches
+    let boardAsColumns = getBoardColumnsAs2DArray()
+    let rowMatches = getAllMatchingPositionsOnBoard(boardModel)
+    let colMatches = getAllMatchingPositionsOnBoardCol(boardAsColumns)
+    let allMatches = rowMatches.concat(colMatches)
+    console.log(allMatches)
+    allMatches.forEach(matchPosition => {
+        node = getNodeFromBoardModelAt(matchPosition)
+        removeNodeFromBoardModel(node)
+    })
+    console.log(boardModel)
+    console.log(countCapitalsOnBoard())
+    render()
+}
+
+// Search the array for repeating characters
+function getIndexesOfMatchingNodesFromArray(arr) {
+    let colorMap = arr.map(e => {
+        if(e === '-') {
+            return '-'
+        } else {
+            return e.color
+        }
+    })
+    let searchString = colorMap.join('')
+    const searchRegExp = RegExp('y{4,}|r{4,}|b{4,}', 'gi');
+    let searchResult = searchRegExp.exec(searchString)
+    let resultIndexes = []
+    if(searchResult !== null) {   
+        let startingPos = searchResult.index
+        let length = searchRegExp.lastIndex - searchResult.index
+        for(let i = 0; i < length; i++) {
+            resultIndexes.push(startingPos + i)
+        }
+    }
+    // Return the indexes
+    return resultIndexes
+
+}
+
+// Flip the board sideways so I can check columns of a 2D array as if they were a single row.
+function getBoardColumnsAs2DArray() {
+    let mappedArr = []
+    for(let col = 0; col < boardModel[0].length; col++) {
+        let colArray = []
+        for(let row = 0; row < boardModel.length; row++) {
+            colArray.push(boardModel[row][col])
+        }
+        mappedArr.push(colArray)
+    }
+    return mappedArr
+}
+
+function countCapitalsOnBoard() {
+    const searchRegExp = RegExp('Y|R|B', 'g');
+    let boardAsString = ''
+    for(let row of boardModel) {
+        let r = ''
+        for(let col of row) {
+            if(col !== '-') {
+                r += col.color
+            }
+        }
+        boardAsString += r
+    }
+    let searchResults = [...boardAsString.matchAll(searchRegExp)]
+    // In the game this will update the remaining viruses
+    console.log('Remaining Capitals', searchResults.length)
+}
+
+function getPositionObj(row, col) { return { row: row, col: col } }
