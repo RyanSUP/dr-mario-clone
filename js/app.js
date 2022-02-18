019dc8ec9d3b1d8ef968bf499b60d3a124a667c9
R Mo <morici23@gmail.com>
2022-02-17 22:50:11 -0500

    More readme updates.

--- js/app.js
+++ js/app.js
@@ -284,8 +284,8 @@ function init() {
     boardModel = []
     initBoardModel()
     // set starting bad things
-    badThingsCount = (2 + level) * 2
-    badThingsCount = clampNum(badThingsCount, 6, 96)
+    badThingsCount = (2 + level) * 4
+    badThingsCount = clampNum(badThingsCount, 12, 96)
     initBadThings()
     countRemainingBadThings() // this is a bandaid around the issue where Bad thinds can spawn on eachother and alter the visible count
     badThingsMessage.style.visibility = 'visible'
