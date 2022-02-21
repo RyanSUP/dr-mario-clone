6201c28f1ae208634ee151b84cbd85b715be7d97
RMo <morici23@gmail.com>
2022-02-21 02:09:54 -0500

    Reset difficulty.

--- js/app.js
+++ js/app.js
@@ -304,10 +304,8 @@ function init() {
     boardModel = []
     initBoardModel()
     // set starting bad things
-    // badThingsCount = (2 + level) * 4
-    badThingsCount = 1
-
-    // badThingsCount = clampNum(badThingsCount, 12, 40)
+    badThingsCount = (2 + level) * 4
+    badThingsCount = clampNum(badThingsCount, 12, 40)
     initBadThings()
     badThingsMessage.style.visibility = 'visible'
     levelMessage.style.visibility = 'visible'
